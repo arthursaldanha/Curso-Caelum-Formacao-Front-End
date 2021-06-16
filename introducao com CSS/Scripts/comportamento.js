b634b3c06e1ee5d22e7c768e314a6a0b638ec464
Arthur Saldanha <saldanhadev@gmail.com>
2021-06-15 23:52:10 -0300

    Curso Caelum - Introdução

---
+++ introducao com CSS/Scripts/comportamento.js
@@ -0,0 +1,32 @@
+// Comentário de 1 linha
+
+/*
+Comentário
+Multi-linha
+*/
+
+/*
+Javascript - Linguagem de Programação
+
+Sintaxe do Js:
+
+Camada de Armazenamento
+
+var chave = valor;
+let chave = valor;
+const chave = valor;
+
+Camada de Instruções
+
+instrucao();
+instrucao(param);
+instrucao(param1, param2);
+
+contexto.instrucao();
+contexto.instrucao(param);
+contexto.instrucao(param1, param2);
+*/
+
+const escola = "Caelum";
+// alert(escola);
+document.write(escola);
